4698ac4f3dd193e404332c0d0a9323a6f45fb3fd
webdesigner-konvert <webdesigner@konvert.be>
2017-05-03 14:34:23 +0200

    fix maps route reset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Amby.be</title>
 	<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
