943e7ba00fe6ea2a212f93dd5bb30d0c41d07e94
webdesigner-konvert <webdesigner@konvert.be>
2017-09-13 14:17:57 +0200

    update emails

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp-auth.mailprotect.be'),
 
     /*
     |--------------------------------------------------------------------------
