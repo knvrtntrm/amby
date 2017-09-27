62c6c1268757b43fb2b83712020f3c191b32c61f
webdesigner-konvert <webdesigner@konvert.be>
2017-09-27 15:22:49 +0200

    compress images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="Cache-control" content="public">
 	<title>Amby Dienstencheques</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300,400,600">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand:400,700">
 	<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body>
