19fb9c1b2c08816d1ab90e237bfd1b24821e6942
webdesigner-konvert <webdesigner@konvert.be>
2017-09-27 14:51:17 +0200

    compress images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,15 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Cache-control" content="public">
 	<title>Amby Dienstencheques</title>
 	<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-
-	<script>
-        window.Laravel =  <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-        ]); ?>
-    </script>
-
 </head>
 <body>
 
@@ -22,6 +16,13 @@
 			</div>
 		@include('partials.footer')
 	</div>
+	
+
+	<script>
+        window.Laravel =  <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
 
 	<script src="{{ mix('/js/app.js') }}"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCvZSeqrAmWQzwfMXZwE7kz7UMFVeXKJls&callback=app.init"
