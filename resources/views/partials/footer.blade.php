28dda9fb5cd8368878dd81f28a2400ecf996471a
webdesigner-konvert <webdesigner@konvert.be>
2017-10-26 11:48:40 +0200

    change footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,6 +5,7 @@
         <strong>Amby.be</strong>
       </p>
       <p>
+      <p>Amby Dienstencheques, Rijselsestraat 26A - 8500 Kortrijk, BE 0417.198.978, RPR Kortrijk, D.CH.01811</p>
         <a class="icon" href="https://www.facebook.com/AmbyDienstencheques" target="_blank">
           <i class="fa fa-facebook-square footer-fb"></i>
         </a>
